35c930bdf7e7dcee21a3aeb6e3b95631cf97271c
Yanuarprayoga9 <yanuarprayoga.stu@pnc.ac.id>
2024-07-16 23:34:58 +0700

    UPDATE api services

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,23 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
+import { getAccessToken } from './services';
 
-const headers = {
-  Accept: 'application/json',
+const createAPI = (useToken = true) => {
+  const baseURL = `${import.meta.env.VITE_BACKEND_URL}/api`;
+
+  const headers: AxiosRequestConfig['headers'] = {
+    Accept: 'application/json',
+  };
+
+  if (useToken) {
+    headers.Authorization = `Bearer ${getAccessToken()}`;
+  }
+  const api = axios.create({
+    baseURL,
+    headers,
+  });
+
+  return api;
 };
+export const apiWithoutToken = createAPI(false);
+export const apiWithToken = createAPI(true);
 
-const api = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
-  headers,
-});
-
-export default api;
--- src/api/services.ts
+++ src/api/services.ts
@@ -1,30 +1,54 @@
-import axios from "axios";
-import api from "."
+import { apiWithToken, apiWithoutToken } from ".";
 
 export function getAccessToken() {
-    return localStorage.getItem("accessToken");
+    return localStorage.getItem("accessToken") || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWF0IjoxNzIxMTEwMTY5fQ.4gUICvHm3L5ZffkEs5pdG-OBwVitwsvOsgCrN3OO-Fw";
 }
 
-export function putAccessToken(accessToken:string) {
+export function putAccessToken(accessToken: string) {
     return localStorage.setItem("accessToken", accessToken);
 }
 
-
-
-export const getRestaurants = async () => {
+export const login = async (username:string, password:string) => {
     try {
-        const res = await axios.get(`https://restaurant-api-sekawan-media-yanuar.vercel.app/api/restaurants`)
-        return res.data.data;
-
+        const res = await apiWithoutToken.post(`/login`, { username, password });
+        const { accessToken } = res.data;
+        putAccessToken(accessToken);
+        return true;
     } catch (error) {
+        console.error("Login failed:", error);
         return false;
     }
-}
+};
+
+export const logout = () => {
+    localStorage.removeItem("accessToken");
+};
+
+export const getRestaurants = async (show = false) => {
+    try {
+        let limit = 8; 
+        if (show) {
+            limit = 100; 
+        }
+        const res = await apiWithToken.get(`/restaurants`, {
+            params: {
+                limit
+            }
+        });
+        return res.data.data;
+    } catch (error) {
+        console.error("Failed to fetch restaurants:", error);
+        return [];
+    }
+};
+
+
 export const getCategories = async () => {
     try {
-        const res = await api.get(`/category`)
-        return res.data
+        const res = await apiWithToken.get(`/category`);
+        return res.data.categories;
     } catch (error) {
-        return false;
+        console.error("Failed to fetch categories:", error);
+        return [];
     }
-}
+};
